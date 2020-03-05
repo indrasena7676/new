42e488d712f8d263b821594cde04a8f211e4c69e
indrasena7676 <60894884+indrasena7676@users.noreply.github.com>
2020-03-05 20:20:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld{
 
-public static void main(String [] args{
-System.out.println("First Java programe");
-}
+public static void main(String[] args) {
+		
+	System.out.println("indrasena Neelam");
+		
+		
+
+	}
 }
